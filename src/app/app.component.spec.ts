a669ee564dce396981bfeb62f63690cbc6a81a3f
Laszlo Kesik <laszlo@kesik.hu>
2018-10-27 14:39:41 +0200

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'ctl-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ctl-fe');
+    expect(app.title).toEqual('ctl-fe testing');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('ctl-fe');
+    expect(compiled.querySelector('h1').textContent).toContain('ctl-fe testing');
   });
 });
